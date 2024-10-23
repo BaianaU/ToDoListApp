0f1def843c43651615cfeaf5c99e0b950f408fd8
BaianaU <baianausupbaeva@gmail.com>
2024-10-23 13:14:35 +0200

    first commit

---
+++ ToDoListApp/ToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppApp.swift
+//  ToDoListApp
+//
+//  Created by Baiana Usupbaeva on 2024-10-23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ToDoListAppTests/ToDoListAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppTests.swift
+//  ToDoListAppTests
+//
+//  Created by Baiana Usupbaeva on 2024-10-23.
+//
+
+import Testing
+@testable import ToDoListApp
+
+struct ToDoListAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
